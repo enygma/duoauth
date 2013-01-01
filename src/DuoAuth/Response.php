2a51b77ac1df3acce6a719b76d51ca355bca6e8f
Chris Cornutt <enygma@phpdeveloper.org>
2013-01-01 15:08:46 -0600

    adding functionality to Response object

--- src/DuoAuth/Response.php
+++ src/DuoAuth/Response.php
@@ -4,9 +4,34 @@ namespace DuoAuth;
 
 class Response
 {
-    public function __construct()
+    private $success = false;
+    private $body = null;
+
+    public function __construct($response = null)
     {
-        // takes in a Guzzle object
+        if ($response !== null) {
+            $this->setData($response);
+        }
+    }
+
+    public function setData($response)
+    {
+        if ($response->getStatusCode() == '200') {
+            $this->success = true;
+        }
+
+        $body = json_decode($response->getBody(true));
+        $this->body = $body->response;
+    }
+
+    public function success()
+    {
+        return ($this->success == true) ? true : false;
+    }
+
+    public function getBody()
+    {
+        return $this->body;
     }
 }
 
