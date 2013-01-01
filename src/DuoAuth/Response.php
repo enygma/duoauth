b44df1ba46598c263c4a5b0743c722eb728f6140
Chris Cornutt <enygma@phpdeveloper.org>
2013-01-01 15:13:58 -0600

    adding comments and setBody to Response object

--- src/DuoAuth/Response.php
+++ src/DuoAuth/Response.php
@@ -4,9 +4,24 @@ namespace DuoAuth;
 
 class Response
 {
+    /**
+     * Success status for the response
+     * @var boolean
+     */
     private $success = false;
+
+    /**
+     * Body contents for the response
+     * @var object
+     */
     private $body = null;
 
+    /**
+     * Construct the response object
+     *     NOTE: If value is set, populates the object with it (setData)
+     * 
+     * @param object Guzzle Response object
+     */
     public function __construct($response = null)
     {
         if ($response !== null) {
@@ -14,6 +29,11 @@ class Response
         }
     }
 
+    /**
+     * Populate the object with data from the response
+     * 
+     * @param object $response Guzzle response object
+     */
     public function setData($response)
     {
         if ($response->getStatusCode() == '200') {
@@ -21,17 +41,37 @@ class Response
         }
 
         $body = json_decode($response->getBody(true));
-        $this->body = $body->response;
+        $this->setBody($body->response);
     }
 
+    /**
+     * Return the status of the response
+     * 
+     * @return boolean Success/fail of the response
+     */
     public function success()
     {
         return ($this->success == true) ? true : false;
     }
 
+    /**
+     * Get the body for the current response
+     * 
+     * @return object Response contents
+     */
     public function getBody()
     {
         return $this->body;
+    }
+
+    /**
+     * Set the body contents for the request
+     * 
+     * @param object Response body contents
+     */
+    public function setBody($body)
+    {
+        $this->body = $body;
     }
 }
 
