04d2bac9f2c28775c9749be61bf024bc62d6b03c
Chris Cornutt <ccornutt@isightpartners.com>
2013-01-03 07:53:01 -0600

    updating autoloader for tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,7 +2,7 @@
 
 // custom autoloader...simple really :)
 spl_autoload_register(function($className) {
-    $classPath = '../src/'.str_replace('\\', '/', $className).'.php';
+    $classPath = __DIR__.'/../src/'.str_replace('\\', '/', $className).'.php';
     if (is_file($classPath)) {
         require_once $classPath;
     }
