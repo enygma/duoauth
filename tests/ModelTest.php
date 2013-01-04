9944c1699f5b2a2436cd217a1e4680cad55341d4
Chris Cornutt <ccornutt@isightpartners.com>
2013-01-04 09:11:49 -0600

    updated model test with more tests

--- tests/ModelTest.php
+++ tests/ModelTest.php
@@ -22,4 +22,48 @@ class ModelTest extends \PHPUnit_Framework_TestCase
 
         $this->assertEquals($properties, $this->model->getProperties());
     }
+
+    /**
+     * Test the load of valid data
+     */
+    public function testLoadValidData()
+    {
+        $model = new \DuoAuth\Model();
+        $model->setProperties(array(
+            'test1' => array(
+                'type' => 'string'
+            ),
+            'test2' => array(
+                'type' => 'string'
+            )
+        ));
+        $data = array(
+            'test1' => 'foo',
+            'test2' => 'bar'
+        );
+        $model->load($data);
+        $this->assertEquals($model->toArray(), $data);
+    }
+
+    /**
+     * Test that, when the data has more values than the model's properties
+     *     it's not set in the values
+     */
+    public function testLoadInvalidProperty()
+    {
+        $model = new \DuoAuth\Model();
+        $model->setProperties(array(
+            'test1' => array(
+                'type' => 'string'
+            )
+        ));
+        $data = array(
+            'test1' => 'foo',
+            'test2' => 'bar'
+        );
+        $model->load($data);
+        $data = $model->toArray();
+
+        $this->assertEquals(isset($data['test2']), false);
+    }
 }
