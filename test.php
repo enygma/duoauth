ae3836a5e0e99f374d4c8687cde21488212e2561
Chris Cornutt <ccornutt@isightpartners.com>
2013-01-22 13:58:55 -0600

    updating test examples to correctly handle return values

--- test.php
+++ test.php
@@ -4,24 +4,37 @@ require_once 'vendor/autoload.php';
 
 // Finding all users
 $user = new \DuoAuth\User();
-var_dump($user->findAll());
+echo 'find all: '; var_dump($user->findAll());
 
 // Finding a single user
 $user = new \DuoAuth\User();
-var_dump($user->findByUsername('ccornutt'));
+if ($user->findByUsername('ccornutt') == true) {
+    echo 'find by username: '; var_dump();
+    var_dump($user);
+}
+
 // or, alternatively
-var_dump($user->find(array('username' => 'ccornutt')));
+if ($user->find('/admin/v1/users', '\\DuoAuth\\User', array('username' => 'ccornutt'), '\DuoAuth\User') == true) {
+    echo 'find by username (direct): '; var_dump($user);
+    var_dump($user);
+}
 
 // Validating a user's inputted code
 $user = new \DuoAuth\User();
-var_dump($user->findByUsername('ccornutt')->validateCode('user-inputted-code'));
+if ($user->findByUsername('ccornutt') == true) {
+    echo 'validate code: '; var_dump($user->validateCode('user-inputted-code'));
+}
+
 
 // Getting the user's phones
 $user = new \DuoAuth\User();
-var_dump($user->findByUsername('ccornutt')->getPhones());
+if ($user->findByUsername('ccornutt') == true) {
+    echo 'get phones: '; var_dump($user->getPhones());
+}
 
 // Associating a phone with a user
 $user = new \DuoAuth\User();
-$phones = $user->findByUsername('ccornutt')->getPhones();
-var_dump($phone[0]->associate($user)); // yes, I know this just reassigns the phone to the same user...
-
+if ($user->findByUsername('ccornutt') == true) {
+    $phones = $user->getPhones();
+    echo 'associate phone: '; var_dump($phones[0]->associate($user)); // yes, I know this just reassigns the phone to the same user...
+}
