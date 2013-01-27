04d5ba87fc9dc9df455a576f0a10a5dba7134508
Chris Cornutt <enygma@phpdeveloper.org>
2013-01-27 09:50:58 -0600

    adding back in the getRequest method to the Auth integration

--- src/DuoAuth/Integrations/Auth.php
+++ src/DuoAuth/Integrations/Auth.php
@@ -4,5 +4,14 @@ namespace DuoAuth\Integrations;
 
 class Auth extends \DuoAuth\Integration
 {
-    
+    public function getRequest()
+    {
+        $request = new \DuoAuth\Request();
+
+        $request->setHostname($this->getHostname())
+            ->setIntKey($this->getIntegration())
+            ->setSecretKey($this->getSecret());
+
+        return $request;
+    }   
 }
