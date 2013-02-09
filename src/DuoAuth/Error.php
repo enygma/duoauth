bcaaab0de02a54c68ca1b91ddf9559551da26179
Chris Cornutt <enygma@phpdeveloper.org>
2013-02-09 06:32:59 -0600

    adding Error class for getting/setting errors (used for request errors for now)

---
+++ src/DuoAuth/Error.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace DuoAuth;
+
+class Error
+{
+    private static $errors = array();
+
+    /**
+     * Add a new error message to the set
+     * 
+     * @param mixed $msg Message (or data to add to error list)
+     * @param string $key Optional key to set the error on
+     */
+    public static function add($msg, $key = null)
+    {
+        if ($key !== null) {
+            self::$errors[$key] = $msg;
+        } else {
+            self::$errors[] = $msg;
+        }
+    }
+
+    /**
+     * Remove a specific error entry from the set
+     * 
+     * @param string $key Key of record to remove
+     * @return boolean True if found, false if not
+     */
+    public static function remove($key)
+    {
+        if (isset(self::$errors[$key])) {
+            unset(self::$errors[$key]);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Get all of the errors in the list or just a specific one
+     * 
+     * @param string $key Optional key of record to return
+     * @return mixed Either the one record if found or the entire array set
+     */
+    public static function get($key = null)
+    {
+        return (isset(self::$errors[$key])) ? self::$errors[$key] : self::$errors;
+    }
+}
+
+?>
