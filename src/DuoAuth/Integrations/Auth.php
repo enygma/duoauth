fad0350b1feecbd3cf57d700a224efc73bc084fd
Chris Cornutt <enygma@phpdeveloper.org>
2013-02-09 13:59:48 -0600

    removing date addition to normal auth integration (messes up header as it's not needed there)

--- src/DuoAuth/Integrations/Auth.php
+++ src/DuoAuth/Integrations/Auth.php
@@ -16,11 +16,6 @@ class Auth extends \DuoAuth\Integration
             ->setIntKey($this->getIntegration())
             ->setSecretKey($this->getSecret());
 
-        // add the timestamp to the hash options
-        $request->setHashOptions(
-            array('date' => date('r'))
-        );
-
         return $request;
     }
 }
