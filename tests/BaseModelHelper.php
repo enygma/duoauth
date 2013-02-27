d8a91fda5b4b0c801a86180f3c7485e31f7d7670
Chris Cornutt <enygma@phpdeveloper.org>
2013-02-27 11:32:02 -0500

    refactoring the BaseModel helper to make it easier to inject a custom response object

--- tests/BaseModelHelper.php
+++ tests/BaseModelHelper.php
@@ -8,15 +8,17 @@ require_once 'MockResponse.php';
 
 class BaseModelHelper extends \PHPUnit_Framework_TestCase
 {
-    protected function buildMockRequest($data)
+    /**
+     * Build a mock request with the given data
+     * 
+     * @param array $data Data to set in the response
+     * @param \DuoAuth\Response $response Mocked Response object [optional]
+     * @return mocked Request object
+     */
+    protected function buildMockRequest($data, $response = null)
     {
         $mockClient = new MockClient();
-        $response = new \DuoAuth\Response();
-
-        $r = new MockResponse();
-        $r->setBody(json_encode($data));
-        $response->setData($r);
-
+        $response = ($response !== null) ? $response : $this->buildMockResponse($data);
         $request = $this->getMock('\DuoAuth\Request', array('send'), array($mockClient));
 
         $request->expects($this->once())
@@ -26,18 +28,43 @@ class BaseModelHelper extends \PHPUnit_Framework_TestCase
         return $request;
     }
 
+    /**
+     * Build a mock response object to inject into the response
+     * 
+     * @param  array $data Data to set in the response
+     * @return \DuoAuth\Response object
+     */
+    protected function buildMockResponse($data)
+    {
+        $response = new \DuoAuth\Response();
+        $r = new MockResponse();
+
+        $r->setBody(json_encode($data));
+        $response->setData($r);
+
+        return $response;
+    }
+
+    /**
+     * Build a mock of the expected model
+     *
+     * @param string $modelClass Full classname to mock (model-based)
+     * @param object $request Mocked \DuoAuth\Response instance
+     * @param array $properies Additonal properties to set on the model object
+     * @return Mocked model-based object
+     */
     protected function buildMockModel($modelClass, $request, $properies = array())
     {
-        $user = $this->getMock($modelClass, array('getRequest'));
+        $mock = $this->getMock($modelClass, array('getRequest'));
 
-        $user->expects($this->once())
+        $mock->expects($this->once())
             ->method('getRequest')
             ->will($this->returnValue($request));
 
         foreach ($properies as $propertyName => $propertyValue) {
-            $user->$propertyName = $propertyValue;
+            $mock->$propertyName = $propertyValue;
         }
 
-        return $user;
+        return $mock;
     }
 }
