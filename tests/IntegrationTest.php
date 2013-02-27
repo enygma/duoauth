e0f34c7fc26f911e3599d020285847d63bd2715a
Chris Cornutt <enygma@phpdeveloper.org>
2013-02-27 15:12:56 -0500

    adding tests for the Integration class

---
+++ tests/IntegrationTest.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace DuoAuth;
+
+class IntegrationTest extends BaseModelHelper
+{
+    /**
+     * Test the getter/setter for the secret key
+     * @covers \DuoAuth\Integration::getSecret
+     * @covers \DuoAuth\Integration::setSecret
+     */
+    public function testGetSetSecret()
+    {
+        $secret = '12345abcdef';
+        $config = array();
+        $int = new \DuoAuth\Integration($config);
+
+        $int->setSecret($secret);
+        $this->assertEquals($int->getSecret(), $secret);
+    }
+
+    /**
+     * Test the getter/setter for the integration key
+     * @covers \DuoAuth\Integration::getIntegration
+     * @covers \DuoAuth\Integration::setIntegration
+     */
+    public function testGetSetIntegration()
+    {
+        $integration = '12345abcdef';
+        $config = array();
+        $int = new \DuoAuth\Integration($config);
+
+        $int->setIntegration($integration);
+        $this->assertEquals($int->getIntegration(), $integration);
+    }
+
+    /**
+     * Test the getter/setter for the hostname
+     * @covers \DuoAuth\Integration::getHostname
+     * @covers \DuoAuth\Integration::setHostname
+     */
+    public function testGetSetHostname()
+    {
+        $hostname = 'testhost.com';
+        $config = array();
+        $int = new \DuoAuth\Integration($config);
+
+        $int->setHostname($hostname);
+        $this->assertEquals($hostname, $int->getHostname());
+    }
+
+    /**
+     * Test that a successful save behaves correctly
+     * @covers \DuoAuth\Integration::save
+     */
+    public function testSaveIntegrationSuccess()
+    {
+        $greeting = 'howdy';
+        $results = array('response' => array(
+            'greeting' => $greeting
+        ));
+
+        $request = $this->buildMockRequest($results);
+        $int = $this->buildMockModel('\DuoAuth\Integration', $request);
+
+        $result = $int->save();
+        $this->assertTrue($result);
+        $this->assertEquals($int->greeting, $greeting);
+    }
+
+    /**
+     * Test that a failed save behaves correctly
+     * @covers \DuoAuth\Integration::save
+     */
+    public function testSaveIntegrationFail()
+    {
+        $greeting = 'howdy';
+        $results = array('response' => array(
+            'greeting' => $greeting
+        ));
+
+        $response = $this->buildMockResponse($results);
+        $response->setSuccess(false);
+
+        $request = $this->buildMockRequest($results, $response);
+        $int = $this->buildMockModel('\DuoAuth\Integration', $request);
+
+        $result = $int->save();
+        $this->assertFalse($result);
+        $this->assertEquals($int->greeting, null);
+    }
+}
